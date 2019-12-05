3737251db1bcd538721e53d4025adf0bae362d32
Marvin Ma <yma010@ucr.edu>
2019-12-04 16:19:44 -0800

    leetcode105 completed

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,20 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+  if (!preorder || !inorder || preorder.length <= 0 || inorder.length <= 0) {
+    return null;
+  }
 
+  const val = preorder.shift();
+  const node = new TreeNode(val);
+  const index = inorder.indexOf(val);
+  const inorderLeft = inorder.slice(0, index);
+  const inorderRight = inorder.slice(index + 1);
+  const preOrderLeft = preorder.slice(0, inorderLeft.length);
+  const preOrderRight = preorder.slice(inorderLeft.length);
+
+  node.left = buildTree(preOrderLeft, inorderLeft);
+  node.right = buildTree(preOrderRight, inorderRight);
+
+  return node;
 }
